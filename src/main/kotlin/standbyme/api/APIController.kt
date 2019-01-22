3307e99fda442a42255b7682e87dc8a20a2c77f8
StandbyMe <stand-by-me@mail.com>
2019-01-22 19:37:09 +0800

    Connect to STORAGE Service

--- src/main/kotlin/standbyme/api/APIController.kt
+++ src/main/kotlin/standbyme/api/APIController.kt
@@ -1,22 +1,32 @@
 package standbyme.api
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("objects")
-class APIController @Autowired constructor(private val webClientBuilder: WebClient.Builder) {
+class APIController @Autowired constructor(private val webClientBuilder: WebClient.Builder, private val discoveryClient: DiscoveryClient) {
 
-    val webClient = webClientBuilder.baseUrl("http://localhost:8081/objects").build()
+    val webClient = webClientBuilder.build()
 
     @GetMapping("{filename:.+}", produces = arrayOf("application/octet-stream"))
     fun get(@PathVariable filename: String): Mono<ByteArray> {
-        return webClient.get().uri("""/$filename""")
-                .retrieve()
-                .bodyToMono<ByteArray>()
-                .onErrorResume { Mono.empty() }
+        val instances = discoveryClient.getInstances("STORAGE")
+        val urls = instances.map { """${it.uri}/objects/$filename""" }
+        return Flux.fromIterable(urls).flatMap {
+            webClient.get().uri(it)
+                    .retrieve()
+                    .bodyToMono<ByteArray>()
+                    .onErrorResume { Mono.empty() }
+        }.single().onErrorResume { Mono.error(NotFoundException(filename)) }
     }
 }
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+class NotFoundException(message: String) : Exception(message)
